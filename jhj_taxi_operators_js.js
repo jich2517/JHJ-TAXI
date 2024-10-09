37af28bad4d4fd77044b7cd3550c802a14b0519a
jich2517 <jichpolking@gmail.com>
2024-10-09 16:46:40 +0800

    Add files via upload

---
+++ jhj_taxi_operators_js.js
@@ -0,0 +1,38 @@
+document.getElementById("toggle-btn").addEventListener("click", function() {
+    var sidebar = document.getElementById("sidebar");
+    var main = document.getElementById("main");
+
+    sidebar.classList.toggle("collapsed");
+    main.classList.toggle("collapsed");
+});
+
+function updateTime() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    document.getElementById('time').textContent = `${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    grabCursor: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
